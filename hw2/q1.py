416cac94ab7ec93230cf39460d4870c6aa6643bc
aaychen <106anna@gmail.com>
2022-04-13 21:55:31 -0700

    new test cases

--- hw2/q1.py
+++ hw2/q1.py
@@ -1,13 +1,73 @@
 class Stack:
-    __init__(): # initializer
-        # create empty array
+    def __init__(self):
+        self.nums = []
 
-    push(val):
-        # append val to array
+    def push(self, val):
+        self.nums.append(val)
 
-    pop():
-        # use list.pop() function to remove most recently added item to array
-        # return item removed
+    def pop(self):
+        return self.nums.pop()
 
-    peek():
-        # access and return last item in array
+    def peek(self):
+        return self.nums[-1]
+
+if __name__ == '__main__':
+    print("test 1")
+    st = Stack()
+    st.push(3)
+    print(st.peek())
+    st.push(4)
+    st.push(5)
+    print(st.pop())
+    print(st.peek())
+    st.push(6)
+    print(st.pop())
+
+    print("\ntest 2")
+    st2 = Stack()
+    st2.push(1)
+    st2.push(2)
+    st2.push(3)
+    st2.push(10)
+    st2.push(5)
+    print(st2.pop())
+    print(st2.pop())
+    print(st2.pop())
+    print(st2.pop())
+    print(st2.pop())
+
+    print("\ntest 3")
+    st3 = Stack()
+    st3.push(1)
+    st3.push(2)
+    print(st3.peek())
+    print(st3.peek())
+
+    print("\ntest 4")
+    st4 = Stack()
+    st4.push(1)
+    print(st4.peek())
+    print(st4.pop())
+    st4.push(2)
+    print(st4.peek())
+    st4.push(5)
+    print(st4.peek())
+    st4.push(10)
+    print(st4.pop())
+    print(st4.pop())
+    print(st4.pop())
+
+    print("\ntest 5")
+    st5 = Stack()
+    st5.push(1)
+    print(st5.peek())
+    st5.push(3)
+    print(st5.pop())
+    st5.push(4)
+    print(st5.peek())
+    st5.push(10)
+    print(st5.pop())
+    st5.push(20)
+    print(st5.pop())
+    print(st5.pop())
+    print(st5.pop())
