6e65a931f6d5970e35df82719398b9fa2110d2c0
aaychen <106anna@gmail.com>
2022-04-13 17:06:17 -0700

    q1 pseudocode

---
+++ hw2/q1.py
@@ -0,0 +1,13 @@
+class Stack:
+    __init__(): # initializer
+        # create empty array
+
+    push(val):
+        # append val to array
+
+    pop():
+        # use list.pop() function to remove most recently added item to array
+        # return item removed
+
+    peek():
+        # access and return last item in array
