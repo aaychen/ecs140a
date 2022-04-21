b9b7bd216066e8fd58c86c413e8496fc07765cfb
aaychen <106anna@gmail.com>
2022-04-20 21:58:45 -0700

    python actual code

--- hw3/q2.py
+++ hw3/q2.py
@@ -1,24 +1,57 @@
 class SimpleParser:
-    initializer(s):
-        # store input string and call it str_in
-        # store position of current character and call it char_pos
+    def __init__(self, str_in):
+        self.string = str_in
+        self.char_pos = -1
 
-    fun_s():
-        # code for grammar rule of S
-        # call get_next_char() and compare to grammar rule of S
-        # if don't see 'a' or 'b', call fun_x()
-            # if fun_x() returns error, print error
-        # print "Input is valid" if nothing goes wrong
+    def fun_s(self):
+        is_success = False
+        ch = self.get_next_char()
+        if ch == 'a': # handle repeats of 'a'
+            if ch == self.peek_next_char():
+                self.fun_s()
+            else:
+                is_success = self.fun_x()
+        elif ch == 'b':
+            is_success = self.fun_x()
+        else:
+            is_success = self.fun_x()
+
+        if is_success:
+            print("Input is valid")
+        else:
+            print(f"Syntax error at character position {self.char_pos}")
+
+        # if ch == 'a' and ch == self.peek_next_char():
+        #     self.fun_s()
+        # elif self.fun_x():
+        #     print("Input is valid")
+        # else:
+        #     print(f"Syntax error at character position {self.char_pos}")
     
-    fun_x():
-        # code for the grammar rule of X
-        # call get_next_char() and compare to grammar rule of X
-        # if char is not 'c' or 'd', return error
+    
+    def fun_x(self):
+        ch = self.get_next_char()
+        if ch == 'c' or ch == 'd':
+            try:
+                self.get_next_char()
+            except IndexError:
+                return True
+        return False
 
-    get_next_char():
-        # increment char_pos and return character at char_pos in str_in
+    def get_next_char(self):
+        self.char_pos += 1
+        return self.string[self.char_pos]
 
-main():
-    test = "bc"
-    sp = new SimpleParser(test) # create an object of SimpleParser with the test string
-    sp.fun_s() # should print "Input is valid"
+    def peek_next_char(self):
+        return self.string[self.char_pos + 1]
+
+def main():
+    tests = ["bc", "acd", "aaad", "c", "2yz", ""]
+    for i, t in enumerate(tests):
+        sp = SimpleParser(t)
+        print(f'Test {i+1}: "{t}" ')
+        sp.fun_s()
+        print()
+
+if __name__ == '__main__':
+    main()
